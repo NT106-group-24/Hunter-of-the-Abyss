a77e98a64d5a8f1a66c2ddbaa37c4b7d45c87d88
MNhatDo1107 <minhnhatdo64@gmail.com>
2025-04-06 16:25:02 +0700

    Up

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using System.Threading;
+
+public class Login : MonoBehaviour
+{
+    [SerializeField]
+    public TMP_InputField usermameInputField;
+    [SerializeField]
+    public TMP_InputField passwordInputField;
+    [SerializeField]
+    public TMP_InputField confirmPasswordInputField;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
